0845872937f9b46a4c9460a82009beb4be8fab5f
ailsaisnania <ailsa.isn2003@mail.ugm.ac.id>
2022-10-18 09:04:52 +0700

    kategori

---
+++ categories.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UMKM WEBSITE - KELOMPOK 7</title>
+
+    <!-- font dari cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    <!-- css link -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <!-- header awal -->
+    <section class="header">
+        <a href="home.php" class="logo">INI LOGO</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="event.php">Event</a>
+            <a href="kategori.php">Categories</a>
+            <a href="about.php">About Us</a>
+        </nav>
+
+    <!-- menu bar di samping kanan atas -->
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+    <section class="home">
+        <div class="home">
+            
+
+            <div class="slide" style="background:url(https://images.unsplash.com/photo-1576562331281-d09e46af9854?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80)">
+                <div class="content">
+                <h3>Categories</h3>       
+            </div> 
+        </div>
+    </section>
+
+    <!-- header akhir -->
+
+    <!-- kategori awal -->
+    <section class="home-kategori">
+        <h1 class="heading-title"> Categories </h1>
+        
+        <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="https://images.unsplash.com/photo-1543353071-087092ec393a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="ini gambar">
+            </div>
+            <div class="content">
+                <h3>Food And Drink</h3>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="image">
+                <img src="https://images.unsplash.com/photo-1556905055-8f358a7a47b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="ini gambar">
+            </div>
+            <div class="content">
+                <h3>Clothes</h3>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="image">
+                <img src="https://images.unsplash.com/photo-1536924940846-227afb31e2a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=866&q=80" alt="ini gambar">
+            </div>
+            <div class="content">
+                <h3>Art</h3>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="image">
+                <img src="https://images.unsplash.com/photo-1631679706909-1844bbd07221?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=792&q=80" alt="ini gambar">
+            </div>
+            <div class="content">
+                <h3>Furniture</h3>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="image">
+                <img src="https://images.unsplash.com/photo-1615655406736-b37c4fabf923?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80">
+            </div>
+            <div class="content">
+                <h3>Electronic</h3>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="image">
+                <img src="https://images.unsplash.com/photo-1500839941678-aae14dbfae9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=951&q=80" alt="ini gambar">
+            </div>
+
+            <div class="content">
+                <h3>Beauty and Health</h3>
+            </div>
+        </div> 
+        </div>
+    </section>
+    <!-- kategori akhir -->
+
+    <!-- footer awal -->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>INI LOGO</h3>
+            </div>
+ 
+            <div class="box">
+                <h3>contact information</h3>
+                 <a href="#"><i class="fas fa-phone"></i> +62 12345678321 </a>
+                 <a href="#"><i class="fas fa-phone"></i> +62 32187654321 </a>
+                 <a href="#"><i class="fas fa-at"></i> umkmAja@gmail.com </a>
+                 <a href="#"><i class="fas fa-location"></i> Yogyakarta, Indonesia </a>
+            </div>
+ 
+            <div class="box">
+                <h3>our social media</h3>
+                 <a href="home.php"><i class="fab fa-facebook"></i> UMKMAja! </a>
+                 <a href="home.php"><i class="fab fa-twitter"></i> UMKMAja! </a>
+                 <a href="home.php"><i class="fab fa-instagram"></i> UMKMAja! </a>
+            </div>
+ 
+        </div> 
+ 
+        <div class="credit">created by <span>UMKMAja!</span> | All Rights Reserved. </div>
+     </section>
+ 
+     <!-- footer akhir -->
+ 
+ 
+     <!-- javascript link -->
+     <script src="script.js"></script>
+     
+ </body>
+ </html>
---
+++ food and drink.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UMKM WEBSITE - KELOMPOK 7</title>
+
+    <!-- font dari cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    <!-- css link -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <!-- header awal -->
+    <section class="header">
+        <a href="home.php" class="logo">INI LOGO</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="event.php">Event</a>
+            <a href="kategori.php">Categories</a>
+            <a href="about.php">About Us</a>
+        </nav>
+
+    <!-- menu bar di samping kanan atas -->
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+
+    <section class="home">
+        <div class="home">
+            
+
+            <div class="slide" style="background:url(https://images.unsplash.com/photo-1505682750263-f3f9e519c565?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Zm9vZCUyMGJhY2tncm91bmR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60);">
+                <div class="content">
+                <h3>Food and Drink</h3>       
+            </div> 
+        </div>
+    </section>
+
+    <!-- header akhir -->
+
+    <!-- kategori awal -->
+    <section class="home-kategori">
+
+        <div class="box-container">
+
+
+        <div class="box">
+            <div class="image">
+                <img src="" alt="ini gambar">
+            </div>
+            <div class="content">
+                <h3>Store name</h3>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="image">
+                <img src="" alt="ini gambar">
+            </div>
+            <div class="content">
+                <h3>Store name</h3>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="image">
+                <img src="" alt="ini gambar">
+            </div>
+            <div class="content">
+                <h3>Store name</h3>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="image">
+                <img src="" alt="ini gambar">
+            </div>
+            <div class="content">
+                <h3>Store name</h3>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="image">
+                <img src="">
+            </div>
+            <div class="content">
+                <h3>Store name</h3>
+            </div>
+        </div> 
+
+        <div class="box">
+            <div class="image">
+                <img src="">
+            </div>
+
+            <div class="content">
+                <h3>Store name</h3>
+            </div>
+        </div> 
+        </div>
+    </section>
+    <!-- kategori akhir -->
+
+
+    
+    <div class="box" style=" width: 200px; margin: 0 auto; margin-bottom: 5rem;">
+        <a href="about.php" class="btn" style="border-radius: 20px;">Read More</a>
+    </div>
+
+
+    <!-- footer awal -->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>INI LOGO</h3>
+            </div>
+ 
+            <div class="box">
+                <h3>contact information</h3>
+                 <a href="#"><i class="fas fa-phone"></i> +62 12345678321 </a>
+                 <a href="#"><i class="fas fa-phone"></i> +62 32187654321 </a>
+                 <a href="#"><i class="fas fa-at"></i> umkmAja@gmail.com </a>
+                 <a href="#"><i class="fas fa-location"></i> Yogyakarta, Indonesia </a>
+            </div>
+ 
+            <div class="box">
+                <h3>our social media</h3>
+                 <a href="home.php"><i class="fab fa-facebook"></i> UMKMAja! </a>
+                 <a href="home.php"><i class="fab fa-twitter"></i> UMKMAja! </a>
+                 <a href="home.php"><i class="fab fa-instagram"></i> UMKMAja! </a>
+            </div>
+ 
+        </div> 
+ 
+        <div class="credit">created by <span>UMKMAja!</span> | All Rights Reserved. </div>
+     </section>
+ 
+     <!-- footer akhir -->
+ 
+ 
+     <!-- javascript link -->
+     <script src="script.js"></script>
+     
+ </body>
+ </html>
