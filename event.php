6fee051a93389223c6c80ea628d27f9c839fa9bf
Rengganis Putri <rengganis.wahyuaning.putri@mail.ugm.ac.id>
2022-10-19 07:57:25 +0700

    feat: event

---
+++ event.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload Event</title>
+
+    <!-- font dari cdn link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    <!-- css link -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- bootstrap-->
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+
+</head>
+<body>
+    <!-- header awal -->
+    <section class="header">
+          <a href="home.php" class="logo">INI LOGO</a>
+
+          <nav class="navbar">
+              <a href="home.php">Home</a>
+              <a href="event.php">Event</a>
+              <a href="kategori.php">Categories</a>
+              <a href="about.php">About Us</a>
+          </nav>
+
+      <!-- menu bar di samping kanan atas -->
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+      </section>
+      <!-- header akhir -->
+
+    <!-- postingan event awal -->
+    <section class="home">
+        <div class="home">
+            <div class="slide" style="background:url(https://images.unsplash.com/photo-1478147427282-58a87a120781?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80); opacity: 60%;">
+                <div class="content">
+                <h3>Event</h3>       
+            </div> 
+        </div>
+    </section>
+
+    <div class="flex-container">
+      <div class="row p-5">
+        <div class="col-6">
+          <img src="https://www.sketchvibes.com/wp-content/uploads/2020/10/aaaaaaaaaaaaaaaaa.jpg"
+          style="width: 500px; height:500px;">
+        </div>
+        <div class="col-6" style="margin-top: -20px;">
+            <p style="font-weight: bold;"> Event 1 </p>
+            <p style="font-size: 16px; line-height: 50px;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            </p>
+            <input type="submit" class="btn px-5" name="submit" value="Readmore" style="font-size: 16px; float: left; display:block; background: #5DCAA0; font-weight:700; border-radius: 20px;">
+        </div>
+      </div>
+    </div>
+    <div class="flex-container">
+      <div class="row p-5" >
+        <div class="col-6" style="margin-top: -20px;">
+            <p style="font-weight: bold;"> Event 2 </p>
+            <p style="font-size: 16px; line-height: 50px;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            </p>
+            <input type="submit" class="btn px-5" name="submit" value="Readmore" style="font-size: 16px; float: left; display:block; background: #5DCAA0; font-weight:700; border-radius: 20px;">
+        </div>
+        <div class="col-6">
+          <img src="" style="width: 500px; height:500px;">
+        </div>
+      </div>
+    </div>
+
+    <div class="flex-container">
+      <div class="row p-5">
+        <div class="col-6">
+          <img src="" style="width: 500px; height:500px;">
+        </div>
+        <div class="col-6" style="margin-top: -20px;">
+            <p style="font-weight: bold;"> Event 3 </p>
+            <p style="font-size: 16px; line-height: 50px;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            </p>
+            <input type="submit" class="btn px-5" name="submit" value="Readmore" style="font-size: 16px; float: left; display:block; background: #5DCAA0; font-weight:700; border-radius: 20px;">
+        </div>
+      </div>
+    </div>
+
+    <div class="flex-container">
+      <div class="row p-5">
+        <div class="col-6" style="margin-top: -20px;">
+            <p style="font-weight: bold;"> Event 4 </p>
+            <p style="font-size: 16px; line-height: 50px;">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            </p>
+            <input type="submit" class="btn px-5" name="submit" value="Readmore" style="font-size: 16px; float: left; display:block; background: #5DCAA0; font-weight:700; border-radius: 20px;">
+        </div>
+        <div class="col-6">
+            <img src="" style="width: 500px; height:500px;">
+        </div>
+      </div>
+    </div>
+    <!-- postingan event akhir -->
+
+    <!-- footer awal -->
+    <section class="footer">
+       <div class="box-container">
+           <div class="box">
+               <h3>INI LOGO</h3>
+           </div>
+
+           <div class="box">
+               <h3>contact information</h3>
+                <a href="#"><i class="fas fa-phone"></i> +62 12345678321 </a>
+                <a href="#"><i class="fas fa-phone"></i> +62 32187654321 </a>
+                <a href="#"><i class="fas fa-at"></i> umkmAja@gmail.com </a>
+                <a href="#"><i class="fas fa-location"></i> Yogyakarta, Indonesia </a>
+           </div>
+
+           <div class="box">
+               <h3>our social media</h3>
+                <a href="home.php"><i class="fab fa-facebook"></i> UMKMAja! </a>
+                <a href="home.php"><i class="fab fa-twitter"></i> UMKMAja! </a>
+                <a href="home.php"><i class="fab fa-instagram"></i> UMKMAja! </a>
+           </div>
+       </div> 
+
+       <div class="credit">created by <span>UMKMAja!</span> | All Rights Reserved. </div>
+    </section>
+    <!-- footer akhir -->
+  
+</body>
+</html>
